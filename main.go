a55c401660ea2dbc083a7e4d33abe3ea214e74ce
Tuss4 <tuss4dbfn@gmail.com>
2015-04-12 09:46:14 -0500

    Format func names.

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ var common = flag.String("c", "", "find common actors")
 var year = flag.String("y", "", "specify a specific year")
 var single_list, list_one, list_two ResponseBody
 
-func build_url(query, year string) url.URL {
+func buildUrl(query, year string) url.URL {
 	var base_query url.URL
 	base_query.Scheme = "http"
 	base_query.Host = "omdbapi.com"
@@ -34,18 +34,18 @@ func build_url(query, year string) url.URL {
 	return base_query
 }
 
-func format_query(q string) string {
+func formatQuery(q string) string {
 	return strings.Join(strings.Split(strings.ToLower(q), " "), "+")
 }
 
-func movie_not_found(movie_name string) {
+func movieNotFound(movie_name string) {
 	fmt.Println("The following movie was not found:", movie_name)
 }
 
-func handle_error_n_status_code(err error, respsonse *http.Response, movie_name string) {
+func handleErrorNStatusCode(err error, respsonse *http.Response, movie_name string) {
 	if err != nil {
 		log.Fatal(err)
-		movie_not_found(movie_name)
+		movieNotFound(movie_name)
 	}
 	if respsonse.StatusCode != 200 {
 		log.Fatal(err)
@@ -54,17 +54,17 @@ func handle_error_n_status_code(err error, respsonse *http.Response, movie_name 
 
 func request(url string, movie_name string, res *ResponseBody) {
 	resp, err := http.Get(url)
-	handle_error_n_status_code(err, resp, movie_name)
+	handleErrorNStatusCode(err, resp, movie_name)
 	defer resp.Body.Close()
 	err = json.NewDecoder(resp.Body).Decode(res)
 }
 
-func compare_request(url_1 string, url_2 string, movie_names []string, res_1 *ResponseBody, res_2 *ResponseBody) {
+func compareRequest(url_1 string, url_2 string, movie_names []string, res_1 *ResponseBody, res_2 *ResponseBody) {
 	request(url_1, movie_names[0], res_1)
 	request(url_2, movie_names[1], res_2)
 }
 
-func find_common(l1, l2 string) []string {
+func findCommon(l1, l2 string) []string {
 	sl1 := strings.Split(l1, ", ")
 	sl2 := strings.Split(l2, ", ")
 	c_actors := make([]string, 0)
@@ -84,22 +84,22 @@ func find_common(l1, l2 string) []string {
 
 func main() {
 	flag.Parse()
-	query := format_query(*search)
+	query := formatQuery(*search)
 	if *search != "" {
-		url := build_url(query, *year)
+		url := buildUrl(query, *year)
 		request(url.String(), *search, &single_list)
 		for _, value := range strings.Split(single_list.Actors, ", ") {
 			fmt.Println(value)
 		}
 	} else if *common != "" {
 		films := strings.Split(*common, ", ")
-		query_1 := format_query(films[0])
-		url_1 := build_url(query_1, "")
-		query_2 := format_query(films[1])
-		url_2 := build_url(query_2, "")
-		compare_request(
+		query_1 := formatQuery(films[0])
+		url_1 := buildUrl(query_1, "")
+		query_2 := formatQuery(films[1])
+		url_2 := buildUrl(query_2, "")
+		compareRequest(
 			url_1.String(), url_2.String(), films, &list_one, &list_two)
-		common_actors := find_common(list_one.Actors, list_two.Actors)
+		common_actors := findCommon(list_one.Actors, list_two.Actors)
 		fmt.Println(films[0], "and", films[1], "have the following actor(s) in common:")
 		for _, value := range common_actors {
 			fmt.Println(value)
